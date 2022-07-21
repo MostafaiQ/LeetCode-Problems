d8f93c3734df3f2c2de8e99b57df568f2b05bafe
Mostafa__iQ <58591829+MostafaiQ@users.noreply.github.com>
2022-07-22 00:23:34 +0300

    Time: 53 ms (37.63%), Space: 13.9 MB (24.32%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,17 +1,18 @@
+from collections import deque
 class Solution:
     def isValid(self, s: str) -> bool:
-        arr = []
+        stack = deque()
         opening = ['{', '[', '(']
         closing = ['}', ']', ')']
         hashMap = {']': '[', '}': '{', ')': '('}
         for i in range(len(s)):
             if s[i] in opening:
-                arr.append(s[i])
+                stack.append(s[i])
                 # print('added', s[i])
             elif s[i] in closing:
-                if len(arr) != 0 and arr[-1] == hashMap.get(s[i]):
-                    arr.pop()
+                if len(stack) != 0 and stack[-1] == hashMap.get(s[i]):
+                    stack.pop()
                     # print('popped', s[i])
                 else:
                     return False
-        return len(arr) == 0
+        return len(stack) == 0
