16349810bbabb57f0662adaf2dd926b14ef913a6
Mostafa__iQ <58591829+MostafaiQ@users.noreply.github.com>
2022-07-22 00:31:21 +0300

    Time: 43 ms (64.77%), Space: 14 MB (24.32%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -8,11 +8,9 @@ class Solution:
         for i in range(len(s)):
             if s[i] in opening:
                 stack.append(s[i])
-                # print('added', s[i])
             elif s[i] in closing:
                 if len(stack) != 0 and stack[-1] == hashMap.get(s[i]):
                     stack.pop()
-                    # print('popped', s[i])
                 else:
                     return False
         return len(stack) == 0
