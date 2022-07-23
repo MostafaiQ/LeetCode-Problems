f0c70866f29672284c6bc5efa622dabfaafb2f60
Mostafa__iQ <58591829+MostafaiQ@users.noreply.github.com>
2022-07-23 12:56:29 +0300

    Time: 1079 ms (92.75%), Space: 25 MB (85.25%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,11 @@
+# Greedy Solution
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l = 0
-        r = 1
-        maxP = 0
-        while r < len(prices):
-            profit = prices[r] - prices[l]
-            if prices[l] < prices[r]:
-                maxP = max(maxP, profit)
-            else:
-                l = r
-            r += 1
-        else:
-            return maxP
+        buyStock = float('inf')
+        finalAnswer = 0
+        for currentStock in prices:
+            buyStock = min(buyStock, currentStock)
+            finalAnswer = max(finalAnswer, currentStock - buyStock)
+        return finalAnswer
                 
             
